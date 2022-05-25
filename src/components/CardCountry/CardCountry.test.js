964f6558c52c2c5c279091be2f65e5bfbee24890
EdinNicolas-web <edin_albarracin@outlook.com>
2022-05-24 21:30:49 -0500

    fix config unit test card

--- src/components/CardCountry/CardCountry.test.js
+++ src/components/CardCountry/CardCountry.test.js
@@ -1,11 +1,11 @@
-import React from "react"
-import {render} from "@testing-library/dom"
-import "@testing-library/jest-dom/extend-expect"
+import React from "react";
+import { render } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
 
 // component
-import CardCountry from "./CardCountry"
+import CardCountry from "./CardCountry";
 
 test("<CardCountry /> Charge correctly Card", () => {
-  render(<CardCountry />)
-  expect(screen.getByText("Albania")).toBeInTheDocument()
-})
+  render(<CardCountry />);
+  expect(screen.getByText("Population:")).toBeInTheDocument();
+});
